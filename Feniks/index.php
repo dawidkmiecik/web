8f22776476758ef56f1abf4db16dc567181587a1
dawidkmiecik <dawidkmiecik@vp.pl>
2015-10-06 02:46:34 +0200

    d

---
+++ Feniks/index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Feniks Desgin - Main</title>
+		
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/fonts.css" rel="stylesheet">
+
+
+
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="js/transition.js"></script>
+		<script src="js/affix.js"></script>
+		<script src="js/alert.js"></script>
+		<script src="js/button.js"></script>
+		<script src="js/carousel.js"></script>
+		<script src="js/collapse.js"></script>
+		<script src="js/modal.js"></script>
+		<script src="js/popover.js"></script>
+		<script src="js/dropdown.js"></script>
+		<script src="js/tab.js"></script>
+		<script src="js/tooltip.js"></script>
+		<script src="js/transition.js"></script>
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+
+					<p class="text-right">Zadzwoń: 794 667 074</p>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Brand</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Start</a></li>
+							<li class="dropdown">
+							  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Oferta<span class="caret"></span></a>
+							  <ul class="dropdown-menu">
+							    <li><a href="#">Strony internetowe</a></li>
+							    <li><a href="#">Sklepy internetowe</a></li>
+							    <li><a href="#">Audyty</a></li>
+							    <li role="separator" class="divider"></li>
+							    <li><a href="#">E-marketing</a></li>
+							  </ul>
+							</li>
+							<li><a href="#">O nas</a></li>
+							<li><a href="#">Kontakt</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</div>
+		
+
+			<img class="img-responsive" src="img/slang.jpg" alt="">
+
+
+		<div id="carousel-example-generic" class="carousel slide container" data-ride="carousel">
+		    <!-- Indicators -->
+			<div class="carousel-indicators row">
+				<div class="col-md-3">
+					<h4>
+						Strony internetowe
+					</h4>
+					<span class="web" data-target="#carousel-example-generic" data-slide-to="0" ></span>
+				</div>
+
+				<div class="col-md-3">
+					<h4>
+						Sklepy internetowe
+					</h4>
+					<span class="shop" data-target="#carousel-example-generic" data-slide-to="1" ></span>
+				</div>
+
+				<div class="col-md-3">
+					<h4>
+						Audyty
+					</h4>
+					<span class="audit" data-target="#carousel-example-generic" data-slide-to="2" ></span>
+				</div>
+
+				<div class="col-md-3">
+					<h4>
+						E-marketing
+					</h4>	
+					<span class="marketing" data-target="#carousel-example-generic" data-slide-to="3" ></span>	
+				</div>
+			</div>
+
+			<!-- End Indicators -->
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+
+				<div class="item active">
+					<div class="media">
+						<div class="media-left">
+							<a href="#">
+							<img class="media-object" src="img/web-slide.png" alt="...">
+							</a>
+						</div>
+						<div class="media-body">
+							<h1 class="media-heading">
+								Strony internetowe
+							</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident in voluptatibus placeat aspernatur temporibus, voluptatum, cupiditate quia, quasi vero officiis natus optio voluptates culpa ullam possimus. Velit voluptas iste numquam, ipsa, ex adipisci corrupti doloribus in quibusdam dignissimos, ad distinctio quasi sequi quod quo minima. Assumenda consectetur necessitatibus iusto aliquid!
+							</p>
+								<a href=""><button type="button" class="btn btn-default btn-lg">Więcej</button></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<div class="media">
+						<div class="media-left">
+							<a href="#">
+							<img class="media-object" src="img/shop-slide.png" alt="...">
+							</a>
+						</div>
+						<div class="media-body">
+							<h1 class="media-heading">
+								Sklepy internetowe
+							</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident in voluptatibus placeat aspernatur temporibus, voluptatum, cupiditate quia, quasi vero officiis natus optio voluptates culpa ullam possimus. Velit voluptas iste numquam, ipsa, ex adipisci corrupti doloribus in quibusdam dignissimos, ad distinctio quasi sequi quod quo minima. Assumenda consectetur necessitatibus iusto aliquid!
+							</p>
+								<a href=""><button type="button" class="btn btn-default btn-lg">Więcej</button></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<div class="media">
+						<div class="media-left">
+							<a href="#">
+							<img class="media-object" src="img/audit-slide.png" alt="...">
+							</a>
+						</div>
+						<div class="media-body">
+							<h1 class="media-heading">
+								Audyty
+							</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident in voluptatibus placeat aspernatur temporibus, voluptatum, cupiditate quia, quasi vero officiis natus optio voluptates culpa ullam possimus. Velit voluptas iste numquam, ipsa, ex adipisci corrupti doloribus in quibusdam dignissimos, ad distinctio quasi sequi quod quo minima. Assumenda consectetur necessitatibus iusto aliquid!
+							</p>
+								<a href=""><button type="button" class="btn btn-default btn-lg">Więcej</button></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<div class="media">
+						<div class="media-left">
+							<a href="#">
+							<img class="media-object" src="img/marketing-slide.png" alt="...">
+							</a>
+						</div>
+						<div class="media-body">
+							<h1 class="media-heading">
+								E-marketing
+							</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident in voluptatibus placeat aspernatur temporibus, voluptatum, cupiditate quia, quasi vero officiis natus optio voluptates culpa ullam possimus. Velit voluptas iste numquam, ipsa, ex adipisci corrupti doloribus in quibusdam dignissimos, ad distinctio quasi sequi quod quo minima. Assumenda consectetur necessitatibus iusto aliquid!
+							</p>
+								<a href=""><button type="button" class="btn btn-default btn-lg">Więcej</button></a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- End Wrapper for slides -->	
+
+			<!-- Controls -->
+			<div class="row">
+			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+			</a>
+			</div>
+			<!-- End Controls -->
+		</div><!-- carousel-example-generic -->
+		<!-- About section -->
+		<section id="BackgroundAbout">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="text-center dupa">O nas</h2>
+					</div>
+					
+					<div class="col-md-6">
+						<img class="img-responsive" src="img/our.jpg" alt="">
+					</div>
+					<div class="col-md-6">
+						<p>
+							Nasze początki sięgają 2010 roku. Od tego mometu tworzymy zgrany i kreatwyny zespół, który projektuje i tworzy strony internetowe przy zachowaniu najwyższych standartów. Do każdego projektu podchodzimy indywidualnie i słuchamy naszych klientów. Przy projektowaniu kierujemy się najnowszymi trendami w internecie oraz technologiami. Potrafimy dostosować stronę klieta do tego w jakie branży działa, zachowując przy tym estyczność, funkcjonalość i czytelność. Nasze strony są responsywne, co w dobie rosnący urządzeń mobilnych jest nie odłączym elementem, każdej witryny. Zawsze stawiamy na zadowolenie i statysfackje klienta.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
+
+		
---
+++ Feniks/js/index.php
@@ -0,0 +1,4 @@
+<?php include 'include/nav.php'; ?>
+<?php include 'include/carousel.php'; ?>
+<?php include 'include/main/main_in.php'; ?>
+<?php include 'include/footer.php'; ?>
