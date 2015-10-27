3c2555e64f304ea85e5aa15cc3034c4d9cdf8419
dawidkmiecik <dawidkmiecik@vp.pl>
2015-10-27 16:32:01 +0100

    d

--- web/include/slider/slider.php
+++ web/include/slider/slider.php
@@ -1,6 +1,6 @@
 		<div id="owl-demo" class="owl-carousel owl-theme">
-		 	<div class="item"><img src="img/wordpress.jpg" alt="The Last of us">
-				<h3>
+		 	<div class="item"><img src="img/wordpress-slider.jpg" alt="The Last of us">
+				<div class="sliderContent">
 					<span class="letter">d</span>
 					<span class="letter">d</span>
 					<span class="letter">s</span>
@@ -10,14 +10,17 @@
 					<span class="letter">g</span>
 					<span class="letter">g</span>
 					<span class="letter">g</span>
-					<span class="letter">g</span>
-				</h3>
+					<span class="letter">g</span>		
+				</div>
+					<div class="wordpressLogoSlider">
+						<img src="img/wordpress-technology.png" alt="">
+					</div>
 				<div class="bar">
 					
 				</div>
 		    </div>
 		    <div class="item"><img src="img/macbook.jpg" alt="The Last of us">
-			  	<h3>
+			  	<div class="sliderContent">
 			  		<span class="letter">d</span>
 			  		<span class="letter">d</span>
 			  		<span class="letter">s</span>
@@ -28,13 +31,14 @@
 			  		<span class="letter">g</span>
 			  		<span class="letter">g</span>
 			  		<span class="letter">g</span>
-			  	</h3>
+			  	</div>
+
 			  	<div class="bar">
 					
 				</div>
 		    </div>
 		    <div class="item"><img src="img/code.jpg" alt="The Last of us">
-			  	<h3>
+			  	<div class="sliderContent">
 			  		<span class="letter">d</span>
 			  		<span class="letter">d</span>
 			  		<span class="letter">s</span>
@@ -45,7 +49,7 @@
 			  		<span class="letter">g</span>
 			  		<span class="letter">g</span>
 			  		<span class="letter">g</span>
-			  	</h3>
+			  	</div>
 			  	<div class="bar">
 					
 				</div>
