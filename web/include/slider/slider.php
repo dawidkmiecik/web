166f64ddb80f2bc4d70da62c6f3c26aedbb3a6c6
dawidkmiecik <dawidkmiecik@vp.pl>
2015-10-28 19:01:34 +0100

    s

--- web/include/slider/slider.php
+++ web/include/slider/slider.php
@@ -1,16 +1,48 @@
 		<div id="owl-demo" class="owl-carousel owl-theme">
 		 	<div class="item"><img src="img/wordpress-slider.jpg" alt="The Last of us">
 				<div class="sliderContent">
+					<span class="letter">W</span>
+					<span class="letter">o</span>
+					<span class="letter">r</span>
 					<span class="letter">d</span>
-					<span class="letter">d</span>
+					<span class="letter">p</span>
+					<span class="letter">r</span>
+					<span class="letter">e</span>
 					<span class="letter">s</span>
-					<span class="letter">a</span>
-					<span class="letter">g</span>
-					<span class="letter">g</span>
-					<span class="letter">g</span>
-					<span class="letter">g</span>
-					<span class="letter">g</span>
-					<span class="letter">g</span>		
+					<span class="letter">s</span>
+				</div>
+				<div class="sliderContentTwo">
+					<span class="letterTwo">i</span>
+					<span class="letterTwo">o</span>
+					<span class="letterTwo">b</span>
+					<span class="letterTwo">s</span>
+					<span class="letterTwo">ł</span>
+					<span class="letterTwo">u</span>
+					<span class="letterTwo">g</span>
+					<span class="letterTwo">s</span>
+					<span class="letterTwo">t</span>
+					<span class="letterTwo">r</span>
+					<span class="letterTwo">o</span>		
+					<span class="letterTwo">n</span>
+					<span class="letterTwo">y</span>
+				</div>
+				<div class="sliderContentThree">
+					<span class="letterThree">s</span>
+					<span class="letterThree">t</span>
+					<span class="letterThree">a</span>
+					<span class="letterThree">j</span>
+					<span class="letterThree">e</span>
+					<span class="letterThree">s</span>
+					<span class="letterThree">i</span>
+					<span class="letterThree">ę</span>
+					<span class="letterThree">p</span>
+					<span class="letterThree">r</span>
+					<span class="letterThree">o</span>
+					<span class="letterThree">s</span>
+					<span class="letterThree">t</span>
+					<span class="letterThree">r</span>
+					<span class="letterThree">z</span>
+					<span class="letterThree">a</span>
 				</div>
 					<div class="wordpressLogoSlider">
 						<img src="img/wordpress-technology.png" alt="">
