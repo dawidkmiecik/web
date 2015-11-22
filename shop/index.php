42ded70f14a94c138e570537bf29813f833882e2
dawidkmiecik <dawidkmiecik@vp.pl>
2015-11-22 23:41:09 +0100

    Version alpha

--- shop/index.php
+++ shop/index.php
@@ -38,6 +38,7 @@
 		  ga('send', 'pageview');
 
 		</script>
+		<script src="js/prefixfree.min.js"></script>
 
 	</head>
 	<body>
@@ -98,10 +99,22 @@
 			<div class="row">
 				<div class="col-md-12 text-center">
 
-					<div class="btn-middle">
-					<h2>Lorem ipsum dolor sit amet.</h2>
-					<a class="btn btn-top" href="#" role="button">Demo sklepu</a>
-					<a class="btn btn-top" href="#" role="button">Sprawdź ofertę</a>
+
+
+					<div id="canvas-container">
+						
+					</div>
+
+					<div class="btnContent">
+						<h2>
+							Lorem ipsum dolor sit amet.
+						</h2>
+						<a class="btn btn-top" TARGET="_blank" href="https://demo.prestashop.com/pl/?view=front" role="button">
+							Demo sklepu
+						</a>
+						<a class="btn btn-top" href="#" role="button">
+							Sprawdź ofertę
+						</a>
 					</div>
 				</div>
 			</div>
@@ -110,76 +123,106 @@
 		</section>
 		
 		<section id="offer">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
+			<div>
+				<!-- Nav tabs -->
+				<ul class="nav nav-tabs nav-justified" role="tablist">
+					<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+					<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+					<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+					<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+				</ul>
 
-					<div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<!-- Tab panes -->
+							<div class="tab-content offerContent">
+								<div role="tabpanel" class="tab-pane active" id="home">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda ratione iure nostrum fuga accusamus rem at, impedit, odio illo autem alias possimus, natus unde! Quo iste, inventore quia! Dignissimos, velit. Veritatis voluptas, et dicta assumenda tempora laborum delectus iusto adipisci!...</div>
+								<div role="tabpanel" class="tab-pane" id="profile">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum eos, magni nobis, pariatur culpa quibusdam nulla fugiat in omnis maiores quaerat aliquid hic beatae optio corporis, praesentium nam temporibus molestiae!</div>
+								<div role="tabpanel" class="tab-pane" id="messages">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, veniam possimus error quibusdam odio accusamus, rerum natus eos reiciendis doloribus!...</div>
+								<div role="tabpanel" class="tab-pane" id="settings">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, voluptatibus, nemo! Ipsa necessitatibus itaque reprehenderit, eveniet at reiciendis, hic accusamus odio eum iusto qui, recusandae? Optio culpa rerum animi, reiciendis commodi magni aliquid quia nostrum perspiciatis, obcaecati vero consequuntur aliquam doloremque nihil facilis velit deleniti quidem aperiam harum inventore. Libero!</div>
+							</div>
+						</div><!-- col-md-12 -->
+					</div><!-- row -->
+				</div><!-- container -->
+			</div>
+		</section>
 
-						<!-- Nav tabs -->
-						<ul class="nav nav-tabs nav-justified" role="tablist">
-							<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-							<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-							<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-							<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
-						</ul>
+		<section id="promotion">
+			<div class="container">
+				<div class="row promotionContent">
+					<h3 class="text-center">Lorem ipsum dolor.</h3>
+					<div class="col-md-4">
+						<img src="http://dummyimage.com/600x400/000/fff" class="img-responsive" alt="">
+					</div>
 
-						<!-- Tab panes -->
-						<div class="tab-content">
-							<div role="tabpanel" class="tab-pane active" id="home">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda ratione iure nostrum fuga accusamus rem at, impedit, odio illo autem alias possimus, natus unde! Quo iste, inventore quia! Dignissimos, velit. Veritatis voluptas, et dicta assumenda tempora laborum delectus iusto adipisci!...</div>
-							<div role="tabpanel" class="tab-pane" id="profile">...</div>
-							<div role="tabpanel" class="tab-pane" id="messages">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, veniam possimus error quibusdam odio accusamus, rerum natus eos reiciendis doloribus!...</div>
-							<div role="tabpanel" class="tab-pane" id="settings">...</div>
+					<div class="col-md-8">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti tenetur dignissimos, atque est officia. Necessitatibus corrupti, asperiores accusamus, facilis veritatis nulla illum iure. Asperiores veritatis vitae quo itaque, non debitis.</p>
+					</div>
+				</div><!-- row -->
+
+				<div class="row promotionContent">
+					<h3 class="text-center">Lorem ipsum dolor.</h3>
+					<div class="col-md-4">
+						<img src="http://dummyimage.com/600x400/000/fff" class="img-responsive" alt="">
+					</div>
+
+					<div class="col-md-8">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis sunt in rem eligendi id! Eos placeat, eligendi quas rem velit culpa voluptates, voluptatibus, quo cupiditate, iste vitae unde saepe provident.</p>
+					</div>
+				</div><!-- row -->
+
+				<div class="row promotionContent">
+					<h3 class="text-center">Lorem ipsum dolor.</h3>
+					<div class="col-md-4">
+						<img src="http://dummyimage.com/600x400/000/fff" class="img-responsive" alt="">
+					</div>
+
+					<div class="col-md-8">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum in sequi debitis facere, tempora porro dolorem dolorum similique, quae quia aliquam omnis laborum assumenda alias. Eos asperiores, suscipit at velit.</p>
+					</div>
+				</div><!-- row -->
+			</div><!-- container -->
+		</section>
+		
+		<section id="service">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+
+						<h2 class="text-center serviceHeading">Usługi</h2>
+
+						<div class="col-sm-4 serviceContent">
+							<img src="img/web-slide.png" class="img-responsive center-block" alt="">
+							<p>
+								Z nami Twoja firma zyska swoje miejsce w sieci, które pomoże klientom w wyborze właśnie Twojego produktu czy usługi. Będzie Twoją wizytówką w internecie, miejscem gdzie klient uzyska wszelkie potrzebne informację, które pomogą mu w dokonaniu wyboru. Z naszym wsparciem, doświadczeniem i zaangażowanie Twoja firma będzie się wspinać na wyżyny. Oczywiście gwarantujemy również szkolenie z zakresu obsługi klienta,   jak i prowadzenia strony.
+							</p>
+							<a href="http://web.feniksdesign.pl"><button type="button" class="btn btn-style btn-lg">Więcej</button></a>
+						</div>
+
+						<div class="col-sm-4 serviceContent">
+							<img src="img/audit-slide.png" class="img-responsive center-block" alt="">
+							<p>
+								Spada Ci sprzedaż ? Coraz mnie ludzi wchodzi na Twoją stronę ? Wiele sklepów i stron internetowych, ofert na allegro zawiera błędy, które skutecznie zmniejszają konwersję zakupów czy pożądanej akcji. Dzięki naszemu audytowi pozwolimy Ci wyeliminować błędy i ustrzec się przed popełnieniem innych, co automatycznie zwiększy sprzedaż Twojego produktu czy usługi. Przeszkolimy również Ciebie i Twoich pracowników.
+							</p>
+						</div>
+
+						<div class="col-sm-4 serviceContent">
+							<img src="img/marketing-slide.png" class="img-responsive center-block" alt="">
+							<p>
+								Marzysz o tym, żeby dotrzeć do szerokiego grona potencjalnych klientów i masz przy tym ograniczone środki ? Chciałbyś aby Twój serwis zaczął nabierać na popularności ? Z nami jest to możliwe, jesteśmy gotowi specjalnie dla Ciebie przygotować odpowiednią kampanię internetową czy prowadzić fanpge lub wy pozycjonować stronę.
+							</p>
 						</div>
 
 					</div>
-
-				</div><!-- col-md-12 -->
-			</div><!-- row -->
-		</div><!-- container -->
-		</section></section>
-
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h2>Lorem ipsum dolor.</h2>
-					<div class="col-sm-4">
-						
-					</div>
-					<div class="col-sm-4">
-						
-					</div>
-					<div class="col-sm-4">
-						
-					</div>
-
-				</div><!-- col-md-12 -->
-			</div><!-- row -->
-		</div><!-- container -->
-		
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-
-					<h2>Lorem ipsum dolor.</h2>
-
-					<div class="col-sm-4">
-						
-					</div>
-
-					<div class="col-sm-4">
-						
-					</div>
-
-					<div class="col-sm-4">
-						
-					</div>
-
-				</div>
-			</div><!-- row -->
-		</div><!-- container -->
+				</div><!-- row -->
+			</div><!-- container -->
+		</section>
 		
 <?php include 'include/contact/contact.php'; ?>
+
+<script src="js/index.js"></script>
+
 <?php include 'include/footer.php'; ?>
 
 		
